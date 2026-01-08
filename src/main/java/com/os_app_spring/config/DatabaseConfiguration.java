487aec8a6dbd1f6a00483bb4235b834b0cb09cdf
Gabriel Andrade Cunha <gandradecortez50@gmail.com>
2026-01-08 13:55:58 -0400

    adding config database class

---
+++ src/main/java/com/os_app_spring/config/DatabaseConfiguration.java
@@ -0,0 +1,36 @@
+package com.os_app_spring.config;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        return new HikariDataSource(config);
+    }
+}
+
+
+
